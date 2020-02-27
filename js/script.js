e0122ca48f3605206fd4f5a3044768037bd607f6
Snowager <Colepr24@gmail.com>
2020-02-27 06:47:38 -0700

    make the reset button functional. Add a base pad of 16x16.

--- js/script.js
+++ js/script.js
@@ -1,36 +1,50 @@
 const container = document.querySelector("#container")
 const gridDiv = document.createElement('div');
 const div = document.querySelector("div.divWhite")
-gridSize = prompt("Enter a size for your drawing pad", "16")
-container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
-container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
+const reset = document.querySelector("#gres")
+gridSize = 256
+container.classList.add("gridStart")
+allDiv = document.getElementsByClassName("divWhole");
 
 function makeDiv() {
     let gridDiv = document.createElement('div');
-        gridDiv.classList.add('divWhite');
         gridDiv.setAttribute("id", "etch" + i)
+        gridDiv.classList.add("divWhole")
         container.appendChild(gridDiv);
 }
 
 function divFactory() {
     for (i = 0; i < (gridSize*gridSize); i++) {
         makeDiv() 
-    }    
-}
-
-
+        divW = document.getElementsByClassName("divWhole")
+        divW[i].classList.add("divWhite")
  
-divFactory()
-
-function changeColor() {
-    for (i = 0; i < (gridSize*gridSize); i++) {
-        divW = document.getElementsByClassName("divWhite")
-        divW[i].addEventListener("mouseover", function(){
-            r = event.target;
-            r.classList.remove("divWhite");
-            r.classList.add("divBlack");
-        })
     }
 }
+        
 
+ 
+function changeColor() {
+        for (i = 0; i < (gridSize*gridSize); i++) {
+            divW = document.getElementsByClassName("divWhite")
+            divW[i].addEventListener("mouseover", function(){
+                r = event.target;
+                r.classList.remove("divWhite");
+                r.classList.add("divBlack");
+            })
+        }
+    } 
+
+
+reset.addEventListener("click", function() {
+    container.classList.remove("gridStart");
+    gridSize = prompt("Enter a size for your drawing pad", "16");
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    divFactory();
+    divW[i].classList.toggle("divBlack");
+    changeColor();
+})
+
+divFactory()
 changeColor()
