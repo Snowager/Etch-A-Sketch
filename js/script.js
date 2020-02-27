dc43b966d815a45e671cc26f43356e61cd0f2f3a
Snowager <Colepr24@gmail.com>
2020-02-27 04:30:29 -0700

    give divs etch id, loop through event targets to initiate mouseover.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,33 @@
 const container = document.querySelector("#container")
+const gridDiv = document.createElement('div');
+const div = document.querySelector("div.divWhite")
 
 function makeDiv() {
     let gridDiv = document.createElement('div');
-        gridDiv.classList.add('divBlack');
+        gridDiv.classList.add('divWhite');
+        gridDiv.setAttribute("id", "etch" + i)
         container.appendChild(gridDiv);
 }
 
 function divFactory() {
     for (i = 0; i <= 255; i++) {
         makeDiv() 
+    }    
+}
+
+
+ 
+divFactory()
+
+function changeColor() {
+    for (i = 0; i <= 255; i++) {
+        divW = document.getElementsByClassName("divWhite")
+        divW[i].addEventListener("mouseover", function(){
+            r = event.target;
+            r.classList.remove("divWhite");
+            r.classList.add("divBlack");
+        })
     }
 }
 
-divFactory()
+changeColor()
